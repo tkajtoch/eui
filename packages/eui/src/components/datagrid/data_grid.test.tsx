d3ee433e6f0c3d03efb5be0d43a500dba1435c38
Tomasz Kajtoch <tomasz.kajtoch@elastic.co>
2025-08-20 17:38:30 +0200

    test(EuiDataGrid): remove unnecessary code from `data_grid.test.tsx`

--- packages/eui/src/components/datagrid/data_grid.test.tsx
+++ packages/eui/src/components/datagrid/data_grid.test.tsx
@@ -7,7 +7,6 @@
  */
 
 import React from 'react';
-import { ReactWrapper } from 'enzyme';
 import { fireEvent } from '@testing-library/react';
 import { requiredProps } from '../../test';
 import { render } from '../../test/rtl';
@@ -15,63 +14,7 @@ import { shouldRenderCustomStyles } from '../../test/internal';
 import { renderCellValueRowAndColumnCount } from './data_grid_test_utils';
 import { EuiDataGrid } from './data_grid';
 
-// Mock the cell popover (TODO: Move failing tests to Cypress and remove need for mock?)
-jest.mock('../popover', () => ({
-  ...jest.requireActual('../popover'),
-  EuiWrappingPopover: ({ children }: { children: React.ReactNode }) => (
-    <div data-test-subj="euiDataGridExpansionPopover">{children}</div>
-  ),
-}));
-
-expect.extend({
-  toBeEuiPopover(received: ReactWrapper) {
-    const pass = received.name() === 'EuiPopover';
-    if (pass) {
-      return {
-        pass: true,
-        message: () =>
-          `expected component "${received.name}" to not be EuiPopover`,
-      };
-    } else {
-      return {
-        pass: false,
-        message: () => `expected component "${received.name}" to be EuiPopover`,
-      };
-    }
-  },
-  euiPopoverToBeOpen(received) {
-    expect(received).toBeEuiPopover();
-    const { isOpen } = received.props();
-    const pass = isOpen === true;
-    if (pass) {
-      return {
-        pass: true,
-        message: () => 'expected EuiPopover to be closed',
-      };
-    } else {
-      return {
-        pass: false,
-        message: () => 'expected EuiPopover to be open',
-      };
-    }
-  },
-});
-declare global {
-  /* eslint-disable-next-line @typescript-eslint/no-namespace,no-redeclare */
-  namespace jest {
-    interface Matchers<R> {
-      toBeEuiPopover(): R;
-      euiPopoverToBeOpen(): R;
-    }
-  }
-}
-
 describe('EuiDataGrid', () => {
-  // Mock requestAnimationFrame to run immediately
-  jest
-    .spyOn(window, 'requestAnimationFrame')
-    .mockImplementation((cb: any) => cb());
-
   shouldRenderCustomStyles(
     <EuiDataGrid
       aria-label=""
